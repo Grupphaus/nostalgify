e4829900f871a0da24ddb9c672b5c6146a327d3c
Federico Pellegrini <fpellegrini@buenosaires.gob.ar>
2019-07-25 17:44:14 -0300

    windows-like menu layout

--- src/app/shared/taskbar/taskbar.component.ts
+++ src/app/shared/taskbar/taskbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-taskbar',
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskbarComponent implements OnInit {
 
+  @Input() public menu = true;
+  public menuList = ['Programs', 'Documents', 'Settings', 'Search', 'Run...'];
+  public appList = ['My-App', 'Playlists', 'Artists', 'Search', 'Recycle'];
+  public documentList = ['asdasd.xls', 'ddd.txt', 'component.css', 'github.png'];
+  public clock: Date;
+  public programsExpand: boolean;
+  public documentsExpand: boolean;
+
   constructor() { }
 
   ngOnInit() {
+      setInterval(() => {
+        this.clock = new Date();
+      }, 1000);
+  }
+
+  toggleMenu() {
+    const isFocused = document.activeElement;
+    const start = document.getElementById('start');
+
+    if (start === isFocused) {
+      this.menu = true;
+    } else {
+      this.menu = false;
+    }
+  }
+
+  expand(nav) {
+    switch (nav) {
+      case 'Programs':
+        this.programsExpand = true;
+        this.documentsExpand = false;
+        break;
+      case 'Documents':
+        this.programsExpand = false;
+        this.documentsExpand = true;
+        break;
+      default:
+        this.programsExpand = false;
+        this.documentsExpand = false;
+        break;
+    }
   }
 
 }
